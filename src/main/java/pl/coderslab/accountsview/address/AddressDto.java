d29f3994ef32adbd326086a343bb19b5f755ac24
arokosandria <kmsa@interia.pl>
2023-05-27 06:40:26 +0200

    Refactor AddressDto

--- src/main/java/pl/coderslab/accountsview/address/AddressDto.java
+++ src/main/java/pl/coderslab/accountsview/address/AddressDto.java
@@ -1,13 +1,14 @@
 package pl.coderslab.accountsview.address;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
-public record AddressDto(Long id,
-                         @NotBlank(message = "numer nie pusty")
-                         String street,
-
-                         Integer number,
-                         Integer numberFlat,
-                         String postCode,
-                         String city) {
+public record AddressDto(
+        @NotNull(message = "id nie moze byc puste")
+        Long id,
+        String street,
+        Integer number,
+        Integer numberFlat,
+        String postCode,
+        String city) {
 }
